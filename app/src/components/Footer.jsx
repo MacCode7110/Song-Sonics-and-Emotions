acd3b3a52fa9997f4a730887761bbd8d16ee3d31
Matthew <memcalarney@wpi.edu>
2026-07-09 21:13:10 -0400

    Correct className reference in the Footer component

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
-    return <footer class="footer">
-        <div class="content has-text-centered">
+    return <footer className="footer">
+        <div className="content has-text-centered">
             Matthew McAlarney developed Music Preferences And Feelings as a web application to understand the relationship between music preferences and the construction of associated feelings.
         </div>
     </footer>
