b46d5da0cb134fca2d77ec32498290b810d02601
Matthew <memcalarney@wpi.edu>
2026-07-10 01:11:25 -0400

    Enhance detail in Footer.jsx

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
      return (
           <footer className="footer has-background-grey-lighter">
                <div className="content has-text-centered has-text-weight-medium is-size-5">
-                    Matthew McAlarney developed Music Preferences and
-                    Feelings to understand the relationship between music
+                    <strong>Matthew McAlarney</strong> (Web Developer, Data Researcher, Scratch DJ, Rapper) developed <em>Music Preferences and
+                    Feelings</em> to understand the relationship between music
                     preferences and the construction of feelings.
                </div>
           </footer>
